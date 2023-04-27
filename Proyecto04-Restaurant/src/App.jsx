20e59e7abd0d7b7a4366654081c6108fd247c1a1
Fernando Arriagada <farriagadal@ing.ucsc.cl>
2023-04-27 17:39:08 -0400

    Creacion de Proyecto Restaurant

---
+++ Proyecto04-Restaurant/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import NavBar from './Components/NavBar'
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+    </>
+  )
+}
+
+export default App
