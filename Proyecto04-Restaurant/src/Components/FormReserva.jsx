17e6bba0d2962f82cba0a111fdff9754e848a12d
Fernando Arriagada <farriagadal@ing.ucsc.cl>
2023-05-05 15:55:10 -0400

    avance form reservas

--- Proyecto04-Restaurant/src/Components/FormReserva.jsx
+++ Proyecto04-Restaurant/src/Components/FormReserva.jsx
@@ -5,26 +5,23 @@ import FloatingLabel from 'react-bootstrap/FloatingLabel';
 
 function Formulario() {
     return (
-        <Form className='container'>
+        <Form className='formulario'>
             <Card>
                 <Form.Group className="mb-3" controlId="formBasicNombre">
-                    <Form.Label>Nombre</Form.Label>
                     <Form.Control
                         type="text"
                         id="inputNombre"
-                        placeholder="Ingresar Nombre"
+                        placeholder="Tu nombre"
                     />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicApellido">
-                    <Form.Label>Apellido</Form.Label>
+                <Form.Group className="mb-3" controlId="formBasicTelefono">
                     <Form.Control
-                        type="password"
-                        id="inputApellido"
-                        aria-describedby="passwordHelpBlock"
+                        type="number"
+                        id="inputTelefono"
+                        placeholder="Telefono"
                     />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                    <Form.Label>Correo Electronico</Form.Label>
                     <Form.Control type="email" placeholder="nombre@example.com" />
                 </Form.Group>
                 <Form.Group>
